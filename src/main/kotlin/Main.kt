fb2e3387ce0b9374f1a6865ba880e7fae813908a
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-19 11:57:54 +0100

    Commit init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val number = 5
+    val string = "String"
+    val bool = true
+
+    println("This is a number ${number}, this is a string ${string}, this is a boolean ${bool} ")
+}
